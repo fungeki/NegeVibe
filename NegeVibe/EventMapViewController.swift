3db3b142117a3a05e10dec608ebf1b9dca479ae0
fungeki <fang.society@gmail.com>
2018-11-29 18:02:21 +0200

    negev is on the map! I think

--- NegeVibe/EventMapViewController.swift
+++ NegeVibe/EventMapViewController.swift
@@ -11,12 +11,42 @@ import MapKit
 
 class EventMapViewController: UIViewController {
 
+    //map outlet
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // set initial location in IndNegev
+    let initialLocation = CLLocation(latitude: 31.2051119, longitude: 34.4533182)
     override func viewDidLoad() {
         super.viewDidLoad()
+        //shifts type to hybrid
+        mapView.mapType = .hybrid
+        
+        //centers location
+        centerMapOnLocation(location: initialLocation)
 
-        // Do any additional setup after loading the view.
+        //loading indicator
+        JustHUD.shared.showInView(view: self.view, withHeader: "Loading", andFooter: "Please Wait")
+        //adding artwork
+        getEvents { (events) in
+            //close loading indicator
+            JustHUD.shared.hide()
+            //chosen event
+            let model = events[0]
+            let location = CLLocationCoordinate2D(latitude: model.latitude, longitude: model.longitude)
+            let type = Symbol(withInt: model.type)?.rawValue ?? "unknown type"
+            let indNegev = Artwork(title: model.title, locationName: model.locationname, type: type, coordinate: location)
+            self.mapView.addAnnotation(indNegev)
+        }
+        
     }
     
+    //sets region
+    let regionRadius: CLLocationDistance = 1300
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 
     /*
     // MARK: - Navigation
---
+++ NegeVibe/Utils/Artwork.swift
@@ -0,0 +1,29 @@
+//
+//  Artwork.swift
+//  NegeVibe
+//
+//  Created by Ran Loock on 29/11/2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import MapKit
+
+class Artwork: NSObject, MKAnnotation {
+    let title: String?
+    let locationName: String
+    let discipline: String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, locationName: String, type discipline: String, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.locationName = locationName
+        self.discipline = discipline
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+    
+    var subtitle: String? {
+        return locationName
+    }
+}
