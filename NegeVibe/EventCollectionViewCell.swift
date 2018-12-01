0124ab86b5377bf787b3090487df6cb352aedbbc
Dor tzemach <tzemachdor@gmail.com>
2018-12-01 11:29:53 +0200

    fgf

---
+++ NegeVibe/EventCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  EventCollectionViewCell.swift
+//  NegeVibe
+//
+//  Created by Dor tzemach on 01/12/2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import UIKit
+
+class EventCollectionViewCell: UICollectionViewCell {
+    
+}
