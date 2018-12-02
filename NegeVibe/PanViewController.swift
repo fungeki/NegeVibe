16ee315ca75cb186fb66114cb718c6f71f6fe1b5
fungeki <fang.society@gmail.com>
2018-12-02 16:41:17 +0200

    pan

---
+++ NegeVibe/PanViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PanViewController.swift
+//  NegeVibe
+//
+//  Created by Ran Loock on 02/12/2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import UIKit
+
+class PanViewController: UIViewController {
+
+    @IBOutlet weak var uiiview: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
