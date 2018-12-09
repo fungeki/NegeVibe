95c87937fb08b1e26ea2245a0677b1b969ed5a27
Dor tzemach <tzemachdor@gmail.com>
2018-12-09 17:48:03 +0200

    new

--- NegeVibe/Object/Symbology.swift
+++ NegeVibe/Object/Symbology.swift
@@ -12,13 +12,28 @@ enum Symbol: String, CustomStringConvertible {
     case festival = "Festival"
     case musicFestival = "Music Festival"
     case movieFestival = "Movies Festival"
+    case rockFestival = "Rock Festival"
+    case cityEvent = "City Event"
+    case religionEvent = "Religion Event"
+    case showsGeneral = "Shows General"
+    case otherGeneral = "Other General"
     
     init?(withInt val: Int){
         switch val {
         case 0:
             self = .festival
+        case 1:
+            self = .cityEvent
+        case 2:
+            self = .showsGeneral
         case 3:
             self = .movieFestival
+        case 4:
+            self = .otherGeneral
+        case 5:
+            self = .religionEvent
+        case 6:
+            self = .rockFestival
         default:
             self = .musicFestival
         }
@@ -29,9 +44,21 @@ enum Symbol: String, CustomStringConvertible {
         case .festival:
             return UIImage(named: "festival")!
         case .movieFestival:
-            return UIImage(named: "ic_inSperishin")!
+            return UIImage(named: "pin_movie_festival")!
+        case .cityEvent:
+            return UIImage(named: "pin_city_event")!
+        case .showsGeneral:
+            return UIImage(named: "pin_shows")!
+        case .otherGeneral:
+            return UIImage(named: "pin_other")!
+        case .religionEvent:
+            return UIImage(named: "pin_religion_event")!
+        case .rockFestival:
+            return UIImage(named: "pin_rock_festival")!
+        case .musicFestival:
+            return UIImage(named: "pin_music_festival")!
         default:
-            return UIImage(named: "ic_inDnegev")!
+            return UIImage(named: "pin_rock_festival")!
         }
     }
     
