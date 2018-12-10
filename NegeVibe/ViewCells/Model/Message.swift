58cff34d5cfea374d5028fbd25ee43f4a600be85
Ofir Elias <O.elias84@gmail.com>
2018-12-10 17:26:55 +0200

    add the chat controller

---
+++ NegeVibe/ViewCells/Model/Message.swift
@@ -0,0 +1,12 @@
+//
+//  Message.swift
+//  NegeVibe
+//
+//  Created by Ofir Elias on 08/12/2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+class Message {
+    var sender:String = ""
+    var messageBody = ""
+}
