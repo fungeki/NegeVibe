ddd6780cff41dc7f3e110d72c39b6872440ee937
Rami Tzafon <ramitzafon@macbook-pro-rami.local>
2018-12-16 11:25:13 +0200

    newTableViewController

---
+++ NegeVibe/ViewControllers/EventTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EventTableViewController.swift
+//  NegeVibe
+//
+//  Created by Rami Tzafon on 16.12.2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
