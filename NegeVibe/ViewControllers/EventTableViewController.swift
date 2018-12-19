47109a071dc6812e4cb1ef5dd2d7a3e9afef20e3
Rami Tzafon <ramitzafon@macbook-pro-rami.local>
2018-12-19 12:27:38 +0200

    temp

--- NegeVibe/ViewControllers/EventTableViewController.swift
+++ NegeVibe/ViewControllers/EventTableViewController.swift
@@ -9,47 +9,37 @@
 import UIKit
 
 class EventTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-    
     
     @IBOutlet weak var eventTableView: UITableView!
+    var arrEvent = [Event]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        
-        
-   /*     if EventsLibrary.getInstance().getNumberOfEvents() == 0{
-            getEvents { (arrEvent) in
-                self.arrEvent = arrEvent
-                EventsLibrary.getInstance().setEvents(arrEvent)
-                self.collectionView.reloadData()
-            }
-        } else {
-            self.arrEvent = EventsLibrary.getInstance().getEvents()
+    if EventsLibrary.getInstance().getNumberOfEvents() == 0{
+         getEvents { (arrEvent) in self.arrEvent = arrEvent
+        EventsLibrary.getInstance().setEvents(arrEvent)
+         self.eventTableView.reloadData()
         }
-    }*/
+         } else {
+         self.arrEvent = EventsLibrary.getInstance().getEvents()
+         }
+    }
+   
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return arrEvent.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell.init()
+         return UITableViewCell.init()
     }
-
-   
-
     
     
+    
+
+
 
     /*
     // MARK: - Navigation
@@ -62,4 +52,4 @@ class EventTableViewController: UIViewController, UITableViewDelegate, UITableVi
     */
 
 }
-}
+
