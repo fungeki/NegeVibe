83fef057afb1dfd74a85c37e275c753497e756a6
Rami Tzafon <ramitzafon@macbook-pro-rami.local>
2018-12-19 11:36:54 +0200

    temp

--- NegeVibe/ViewControllers/EventTableViewController.swift
+++ NegeVibe/ViewControllers/EventTableViewController.swift
@@ -9,16 +9,34 @@
 import UIKit
 
 class EventTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
     
     
     @IBOutlet weak var eventTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        eventTableView.delegate = self
-        eventTableView.dataSource = self
+        
         // Do any additional setup after loading the view.
-    }
+        
+        
+   /*     if EventsLibrary.getInstance().getNumberOfEvents() == 0{
+            getEvents { (arrEvent) in
+                self.arrEvent = arrEvent
+                EventsLibrary.getInstance().setEvents(arrEvent)
+                self.collectionView.reloadData()
+            }
+        } else {
+            self.arrEvent = EventsLibrary.getInstance().getEvents()
+        }
+    }*/
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
@@ -43,4 +61,5 @@ class EventTableViewController: UIViewController, UITableViewDelegate, UITableVi
     }
     */
 
+}
 }
