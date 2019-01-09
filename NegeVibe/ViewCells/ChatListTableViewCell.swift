7a7cbb5f136d0325ae67b224630edeb9372edb74
Dor tzemach <dormacbookair@dors-macbook-air.local>
2019-01-09 17:53:13 +0200

    foot

---
+++ NegeVibe/ViewCells/ChatListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatListTableViewCell.swift
+//  NegeVibe
+//
+//  Created by Dor tzemach on 09/01/2019.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+
+class ChatListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
