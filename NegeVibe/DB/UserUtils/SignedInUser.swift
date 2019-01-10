15277583dbb008e08330d5a76b7102f6d0f7b4d4
fungeki <fang.society@gmail.com>
2019-01-10 17:26:24 +0200

    moar user changes

--- NegeVibe/DB/UserUtils/SignedInUser.swift
+++ NegeVibe/DB/UserUtils/SignedInUser.swift
@@ -34,29 +34,6 @@ class SignedInUser{
         }
     }
     
-    private func getUserDetails(completion: @escaping ((_ userChatName: String?)->Void)){
-        let db = Firestore.firestore()
-        DispatchQueue.main.async {
-            guard let user = Auth.auth().currentUser else {
-                return
-            }
-            
-            let uid = user.uid
-            let docRef = db.collection("users").document(uid)
-            docRef.getDocument { (document, error) in
-                if let document = document, document.exists {
-                    //  let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-                    let data = document.data()
-                    if let result = data?["chatName"] as? String{
-                        completion(result)
-                    }
-                } else {
-                    completion(nil)
-                }
-            }
-        }
-    }
-    
     private static var sharedInstance:SignedInUser!
     
     
