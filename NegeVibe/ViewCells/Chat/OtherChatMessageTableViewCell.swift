8626d73ce5e3bb839b57484e95197d982f184161
fungeki <fang.society@gmail.com>
2019-01-18 12:48:27 +0200

    Mitzi has written a chat, woot?

---
+++ NegeVibe/ViewCells/Chat/OtherChatMessageTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  OtherChatMessageTableViewCell.swift
+//  NegeVibe
+//
+//  Created by Ran Loock on 18/01/2019.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+
+class OtherChatMessageTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var textBodyLabel: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var messageWrapperView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        messageWrapperView.roundCorners(corners: [.topLeft, .topRight, .topLeft, .bottomRight], radius: 20)
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- NegeVibe/ViewControllers/ChatViewController.swift
+++ NegeVibe/ViewControllers/ChatViewController.swift
@@ -2,7 +2,7 @@
 //  ChatViewController.swift
 //  NegeVibe
 //
-//  Created by Ran Loock on 07/01/2019.
+//  Created by Ran Loock on 18/01/2019.
 //  Copyright © 2019 test. All rights reserved.
 //
 
@@ -10,39 +10,31 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
-    
-    @IBOutlet weak var messageSenderWrapperView: UIView!
+    let mockupChat = [ChatMessage]()
+    @IBOutlet weak var textWrapperView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+
+    
         // Do any additional setup after loading the view.
     }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        textWrapperView.layer.cornerRadius = textWrapperView.bounds.height / 2
+    }
+    
     
 
-    override func viewDidLayoutSubviews() {
-        messageSenderWrapperView.layer.cornerRadius = messageSenderWrapperView.frame.height / 2
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
-
-extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
+extension ChatViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return mockupChat.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "carpoolCell") as! CarpoolTableViewCell
-        return UITableViewCell.init()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "otherChat") as! OtherChatMessageTableViewCell
+        return cell
     }
     
     
 }
-
